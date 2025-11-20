cd8aeb639bfb927e2c733c2748372a33626d7664
Manishbhai9350 <keshraram294@gmail.com>
2025-11-20 20:30:00 +0530

    Dom Structure & Styling

--- src/main.js
+++ src/main.js
@@ -1,24 +1,92 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+gsap.registerPlugin(SplitText);
 
-setupCounter(document.querySelector('#counter'))
+const TotalImages = 6;
+
+window.addEventListener("DOMContentLoaded", () => {
+  let ScrollValue = 0;
+  let TargetScrollValue = 0;
+  let Velocity = 0;
+  let MaxVelocity = 4;
+  let EasingFactors = {
+    Scroll: 0.001,
+  };
+  let Progress = 0;
+
+  const ImageContainer = document.querySelector(".hero-section .images");
+  const HeroTexts = gsap.utils.toArray(".hero-section .texts h1");
+  const SplitedHeroTexts = [];
+  const AllWords = []
+
+  function AddImages(onLoad = () => {}) {
+    let loaded = 0;
+    ImageContainer.innerHTML = "";
+    for (let i = 1; i <= TotalImages; i++) {
+      const src = `/${i}.jpg`;
+      const imagecon = document.createElement("div");
+      imagecon.classList.add("image");
+      const image = new Image();
+      image.src = src;
+      imagecon.appendChild(image);
+      ImageContainer.appendChild(imagecon);
+      image.onload = () => {
+        loaded++;
+        if (loaded == TotalImages) onLoad();
+      };
+    }
+  }
+  function Split() {
+    HeroTexts.forEach((HeroText, i) => {
+      const Splited = new SplitText(HeroText, {
+        type: ["words", "lines"],
+        wordsClass: `hero-word-${i + 1}`,
+        mask: "lines",
+      });
+      AllWords.push(...Splited.words)
+      SplitedHeroTexts.push(Splited);
+    });
+  }
+  AddImages(Update);
+  Split();
+  gsap.set([".hero-word-2", ".hero-word-3"], {
+    yPercent: 100,
+  });
+
+  let Time = 0;
+
+  function UpdateDom(p = 0) {
+    AllWords.forEach((Word,i) => {
+      const WordP = (i + 1) / AllWords.length;
+      
+    })
+  }
+
+  function Update(t = 0) {
+    let DT = t - Time;
+    Time = t;
+    let PrevScroll = ScrollValue;
+
+    ScrollValue +=
+      (TargetScrollValue - ScrollValue) * DT * EasingFactors.Scroll;
+    Progress = Math.min(Math.max(ScrollValue / 100, 0), 1);
+
+    Velocity = ScrollValue - PrevScroll;
+
+    if (ScrollValue - Math.floor(ScrollValue) >= 0.999) {
+      ScrollValue = Math.ceil(ScrollValue);
+    }
+
+    UpdateDom(Progress);
+
+    requestAnimationFrame(Update);
+  }
+
+  window.addEventListener("wheel", (e) => {
+    TargetScrollValue += e.deltaY / 100;
+    // console.log(Math.max(Math.min(TargetScrollValue,100),0))
+    TargetScrollValue = Math.max(Math.min(TargetScrollValue, 100), 0);
+  });
+});
