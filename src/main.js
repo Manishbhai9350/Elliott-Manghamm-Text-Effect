c310d42b5182f00702fa8193aed413307820cf7d
Manishbhai9350 <keshraram294@gmail.com>
2025-11-21 19:31:10 +0530

    Pending Work

--- src/main.js
+++ src/main.js
@@ -1,30 +1,23 @@
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
 import "./style.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
 
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+const lenis = new Lenis();
+
+lenis.on("scroll", () => ScrollTrigger.update);
+gsap.ticker.add((t) => lenis.raf(t * 1000));
+gsap.ticker.lagSmoothing(0);
 
 const TotalImages = 6;
 
 window.addEventListener("DOMContentLoaded", () => {
-  let ScrollValue = 0;
-  let TargetScrollValue = 0;
-  let Velocity = 0;
-  let MaxVelocity = 4;
-  let EasingFactors = {
-    Scroll: 0.001,
-  };
-  let Progress = 0;
-
   const ImageContainer = document.querySelector(".hero-section .images");
   const HeroTexts = gsap.utils.toArray(".hero-section .texts h1");
   const SplitedHeroWords = [];
-
-  function InRange(t, range = [0, 0]) {
-    if (t >= range[0] && t < range[1])
-      return (t - range[0]) / (range[1] - range[0]);
-    else return -1;
-  }
 
   function AddImages(onLoad = () => {}) {
     let loaded = 0;
@@ -53,75 +46,142 @@ window.addEventListener("DOMContentLoaded", () => {
       SplitedHeroWords.push(Splited.words);
     });
   }
-  AddImages(Update);
+  AddImages();
   Split();
   gsap.set([".hero-word-2", ".hero-word-3"], {
     yPercent: 100,
   });
 
-  let Time = 0;
+  // const TL1 = gsap.timeline({
+  //   onComplete() {
+  //     gsap.to(SplitedHeroWords[0], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[1], {
+  //       yPercent: 0,
+  //     });
+  //     gsap.to(SplitedHeroWords[2], {
+  //       yPercent: 100,
+  //     });
+  //   },
+  //   onReverseComplete(){
+  //     gsap.to(SplitedHeroWords[2], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[1], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[0], {
+  //       yPercent: 0,
+  //     });
+  //   },
+  //   onUpdate(){
+  //     gsap.to(SplitedHeroWords[2], {
+  //       yPercent: 100,
+  //     });
+  //   },
+  //   scrollTrigger: {
+  //     trigger: ".hero-section",
+  //     start: `top top`,
+  //     end: `+=${(innerHeight * 7) / 3}px`,
+  //     markers: {
+  //       startColor: "yellow",
+  //       endColor: "purple",
+  //     },
+  //     scrub:true
+  //   },
+  // });
+  // TL1.to(
+  //   SplitedHeroWords[0],
+  //   {
+  //     yPercent: 100,
+  //     stagger: 0.1,
+  //   }
+  //   // "<"
+  // );
+  // TL1.to(
+  //   SplitedHeroWords[1],
+  //   {
+  //     yPercent: 0,
+  //     stagger: 0.1,
+  //   }
+  //   // "<"
+  // );
+  // const TL2 = gsap.timeline({
+  //   onComplete() {
+  //     gsap.to(SplitedHeroWords[0], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[1], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[2], {
+  //       yPercent: 0,
+  //     });
+  //   },
+  //   onReverseComplete(){
+  //     gsap.to(SplitedHeroWords[2], {
+  //       yPercent: 100,
+  //     });
+  //     gsap.to(SplitedHeroWords[1], {
+  //       yPercent: 0,
+  //     });
+  //     gsap.to(SplitedHeroWords[0], {
+  //       yPercent: 100,
+  //     });
+  //   },
+  //   onUpdate(){
+  //     gsap.to(SplitedHeroWords[0], {
+  //       yPercent: 100,
+  //     });
+  //   },
+  //   scrollTrigger: {
+  //     trigger: ".hero-section",
+  //     start: `top -${(innerHeight * 7) / 2}px`,
+  //     end: `+=${(innerHeight * 7) / 3}px`,
+  //     scrub:true
+  //   },
+  // });
+  // TL2.to(SplitedHeroWords[1], {
+  //   yPercent: 100,
+  //   stagger: 0.1,
+  // });
+  // TL2.to(
+  //   SplitedHeroWords[2],
+  //   {
+  //     yPercent: 0,
+  //     stagger: 0.1,
+  //   }
+  //   // "<"
+  // );
 
-  function UpdateDom(Progress = 0) {
-    if (Progress < 0.0009) {
-      Progress = 0;
-    }
-    if (Progress > 0.99) {
-      Progress = 1;
-    }
-    if (Progress <= 0.5) {
-      let WordsLength = SplitedHeroWords[0].length + SplitedHeroWords[1].length;
-      const Phase2Range = [SplitedHeroWords[0].length / WordsLength / 2,SplitedHeroWords[0].length / 2 / WordsLength + SplitedHeroWords[1].length / WordsLength / 2];
-      const RangedProgress = InRange(Progress, Phase2Range);
-      if (RangedProgress >= 0) {
-        SplitedHeroWords[0].forEach((word, i) => {
-          const totalWords = SplitedHeroWords[0].length;
-          const wordProgress = i / (totalWords - 1);
-          let wordRangedProgress = Math.max(Math.min(Progress - wordProgress,wordProgress),0)
-          console.log(wordRangedProgress / wordProgress * 100)
-          gsap.set(word,{
-            yPercent:wordRangedProgress / wordProgress * 100
-          })
-        });
-      }
-    } else if (Progress > 0.5) {
-      let WordsLength = SplitedHeroWords[1].length + SplitedHeroWords[2].length;
-      const Phase2Range = [
-        0.5 + SplitedHeroWords[1].length / WordsLength / 2,
-        0.5 +
-          SplitedHeroWords[1].length / WordsLength / 2 +
-          SplitedHeroWords[2].length / WordsLength / 2,
-      ];
-      const RangedProgress = InRange(Progress, Phase2Range);
-      if (RangedProgress >= 0) {
-      }
-    }
-  }
-
-  function Update(t = 0) {
-    let DT = t - Time;
-    Time = t;
-    let PrevScroll = ScrollValue;
-
-    ScrollValue +=
-      (TargetScrollValue - ScrollValue) * DT * EasingFactors.Scroll;
-    Progress = Math.min(Math.max(ScrollValue / 100, 0), 1);
-
-    // if(Progress - Math.floor(Progress) >= .99) Progress = Math.ceil(Progress)
-
-    Velocity = ScrollValue - PrevScroll;
-
-    if (ScrollValue - Math.floor(ScrollValue) >= 0.999) {
-      ScrollValue = Math.ceil(ScrollValue);
-    }
-
-    UpdateDom(Progress);
-
-    requestAnimationFrame(Update);
-  }
-
-  window.addEventListener("wheel", (e) => {
-    TargetScrollValue += e.deltaY / 100;
-    // console.log(Math.max(Math.min(TargetScrollValue,100),0))
-    TargetScrollValue = Math.max(Math.min(TargetScrollValue, 100), 0);
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".hero-section",
+      start: "top top",
+      end: `+=${innerHeight * 10}px`,
+      scrub: 2,
+      pin: true,
+      markers: true,
+    },
   });
+
+  // 1️⃣ Word 1 out
+  tl.to(SplitedHeroWords[0], { stagger:.1,yPercent: 100 });
+
+  // 2️⃣ Word 2 in
+  tl.to(SplitedHeroWords[1], { stagger:.1,yPercent: 0 });
+
+  // 3️⃣ Word 2 out
+  tl.to(SplitedHeroWords[1], { stagger:.1,yPercent: 100 });
+
+  // 4️⃣ Word 3 in
+  tl.to(SplitedHeroWords[2], { stagger:.1,yPercent: 0 });
+
+  // ScrollTrigger.create({
+  //   trigger: ".hero-section",
+  //   start: "top top",
+  //   end: `+=${innerHeight * 7}px`,
+  //   pin: true,
+  // });
 });
